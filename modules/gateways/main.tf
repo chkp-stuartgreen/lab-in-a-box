ad33a08b18cc3a5f3057afad0505603cee4b4422
chkp-stuartgreen <stuartg@checkpoint.com>
2021-02-17 15:23:52 +0000

    I *think* the resource deletion issue has been addressed by adding some manual dependencies.

--- modules/gateways/main.tf
+++ modules/gateways/main.tf
@@ -54,7 +54,7 @@ resource "azurerm_network_interface" "nic_vip" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "nic_vip_lb_association" {
-  depends_on              = [azurerm_network_interface.nic_vip, azurerm_lb_backend_address_pool.frontend-lb-pool]
+  depends_on              = [azurerm_network_interface.nic_vip, azurerm_lb_backend_address_pool.frontend-lb-pool, azurerm_virtual_machine.vm-instance-availability-set]
   network_interface_id    = azurerm_network_interface.nic_vip.id
   ip_configuration_name   = "ipconfig1"
   backend_address_pool_id = azurerm_lb_backend_address_pool.frontend-lb-pool.id
